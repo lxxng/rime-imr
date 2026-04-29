4f982f42586ce2996876504c97cec52d1ba8dd43
lxp <lixupeng@jasolar.com>
2026-04-29 09:47:29 +0800

    t93_processor

--- lua/imr/t93_processor.lua
+++ lua/imr/t93_processor.lua
@@ -1,3 +1,24 @@
+---@param _text string
+---@return string rest_text
+local function ignore_word(_text)
+    ---@param text string
+    ---@return boolean is_word
+    ---@return string rest_text
+    local function is_word(text)
+        if text:match('^[1-9][0-9][0-9][a-e]') then
+            return true, text:sub(5)
+        end
+        if text:match('^[1-9][0-9][0-9]') then
+            return true, text:sub(4)
+        end
+        return false, text
+    end
+    local flag, text = true, _text
+    while flag do
+        flag, text = is_word(text)
+    end
+    return text
+end
 local Processor = {
     init = function(env) end,
     func = function(key, env)
@@ -9,27 +30,28 @@ local Processor = {
         then
             local key_repr = key:repr()
             if key_repr == '0' then
-                if
-                    require('imr.utils.pattern').match(context.input, '^([1-9][0-9]{2}[a-e]?)+$')
-                      or
-                    require('imr.utils.pattern').match(context.input, '^([1-9][0-9]{2}[a-e]?)+`$')
-                then
+                local text = ignore_word(context.input)
+                if text == '' or text == '`' then
                     context:push_input('`')
                     return 1
                 end
             end
-            if key_repr == 'a' or key_repr == 'b' or key_repr == 'c' or key_repr == 'd' or key_repr == 'e' then
-                local flag, rest_text = require('imr.utils.pattern').match(context.input, '^([1-9][0-9]{2}[a-e]?)+$')
-                if not flag then
-                    flag, rest_text = require('imr.utils.pattern').match(context.input, '^([1-9][0-9]{2}[a-e]?)+`$')
-                    if rest_text:match('^[1-9]$') then
-                        context:push_input('00' .. key_repr)
-                        return 1
-                    end
-                    if rest_text:match('^[1-9][0-9]$') then
-                        context:push_input('0' .. key_repr)
-                        return 1
-                    end
+            if key_repr:match('^[a-e]$') then
+                local text = ignore_word(context.input)
+                if text:match('^[1-9]$') then
+                    context:push_input('00')
+                    context:push_input(key_repr)
+                    return 1
+                end
+                if text:match('^[1-9][0-9]$') then
+                    context:push_input('0')
+                    context:push_input(key_repr)
+                    return 1
+                end
+                if text == '' and context.input:sub(#context.input, #context.input):match('[a-e]') then
+                    context:pop_input(1)
+                    context:push_input(key_repr)
+                    return 1
                 end
             end
         end
